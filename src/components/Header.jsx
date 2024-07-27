0e90126bb8cbd7a14fff2464096efcb8ec85d639
Bidhun <bidhunvaikkath@gmail.com>
2024-07-27 10:33:07 +0530

    initial file structure and add header compoent

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-Calculator-logo.png";
+export default function Header() {
+  return (
+    <div id="header">
+      <h1>React Investment Calculator</h1>
+      <img src={logo} alt="inestment-calculator-logo" />
+    </div>
+  );
+}
